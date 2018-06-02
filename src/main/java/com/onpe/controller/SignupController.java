28cb922e2042837cd06aa2c84ed75e0228f12a35
Roosevelt <panta_85.x@hotmail.com>
2018-06-02 17:10:43 -0500

    sign up form message

--- src/main/java/com/onpe/controller/SignupController.java
+++ src/main/java/com/onpe/controller/SignupController.java
@@ -9,6 +9,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.onpe.entity.Usuario;
 import com.onpe.others.PasswordEncoder;
@@ -31,8 +32,16 @@ public class SignupController {
 	}
 	
 	@PostMapping
-	public String save(@Valid Usuario usuario, BindingResult result, Model model) {
+	public String save(@Valid Usuario usuario, BindingResult result, Model model, RedirectAttributes redirect) {
 		try {
+			
+			if (usuario.getNombre().length() <= 0 || usuario.getPassword().length() <= 0) {
+				redirect.addFlashAttribute("objResult", true);
+				redirect.addFlashAttribute("resultado", "Campo(s) incompleto(s)");
+				
+				return "redirect:/signup";
+			}
+			
 			
 			String auxpass = usuario.getPassword();
 			usuario.setPassword(encoder.encodePass(auxpass));

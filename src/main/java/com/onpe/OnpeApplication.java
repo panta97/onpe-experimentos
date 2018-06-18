6bc418776d79d991e322b58a7bf25e665c8c3c46
Roosevelt <panta_85.x@hotmail.com>
2018-06-18 16:02:16 -0500

    war build

--- src/main/java/com/onpe/OnpeApplication.java
+++ src/main/java/com/onpe/OnpeApplication.java
@@ -5,19 +5,19 @@ import java.util.Scanner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class OnpeApplication {
+public class OnpeApplication extends SpringBootServletInitializer{
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(OnpeApplication.class);
+    }
 	
 	public static void main(String[] args) {
 		SpringApplication.run(OnpeApplication.class, args);
-		System.out.println("Press 'Enter' to terminate");
-		new Scanner(System.in).nextLine();
-		System.out.println("Exiting");
-		System.exit(1);
 	}
-	
-	
-	
 }
 
